8e929949cac9d0562bf8c482ca7f8f462922b1c5
kirkthomas704 <kirkt@smu.edu>
2018-10-01 19:31:50 -0500

    Update Servomotors.java

--- Servomotors.java
+++ Servomotors.java
@@ -5,21 +5,28 @@ public class Servomotors extends Main {
 	public static RXTXRobot Robot = new ArduinoNano();
 	
 	public static void main(String[] args, int a) {
-		rotate(0);
-		rotate(a);
-		rotate(0);
+		Robot.setPort("COM6");	
+		Robot.connect();
+		
+		Robot.runPCAServo(11, 10 );
+		Robot.sleep(1000);
+		Robot.runPCAServo(11, a );
+		Robot.sleep(1000);
+		Robot.runPCAServo(11, 10 );
+		Robot.sleep(1000);
+		Robot.close();
 	}
 	public static void rotate(int angle) {
 		//robot is the Arduino board instantiated in Main class
 		//sets com port 3 to be input port
-		Robot.setPort("COM5");
-		Robot.setVerbose(true); //debugging
-		Robot.connect();
+		
+		//Robot.setVerbose(true); //debugging
+		
 		//servo is to be wired to digital pin 11
-		Robot.attachServo(RXTXRobot.SERVO1, 11);//connects the servo's format is the servo's name followed by pin #
+		//Robot.attachServo(RXTXRobot.SERVO1, 11);//connects the servo's format is the servo's name followed by pin #
 		//rotates to angle a for time specified by var timeatangle  
-		Robot.runPCAServo(11, angle );
+		
 		//closes out of robot
-		Robot.close();
+		//Robot.close();
 	}
 }
