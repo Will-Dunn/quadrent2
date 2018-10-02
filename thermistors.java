50b27bd1aed07dd1b5b6e490e50cf1ad9efb8457
kirkthomas704 <kirkt@smu.edu>
2018-10-01 19:35:19 -0500

    Update thermistors.java

--- thermistors.java
+++ thermistors.java
@@ -8,14 +8,14 @@ public class thermistors {
     {    
 		RXTXRobot r = new ArduinoNano(); //Create RXTXRobot object
 
-		r.setPort("COM5"); // Sets the port to COM5
+		r.setPort("COM6"); // Sets the port to COM5
 	
 		r.connect();
 
 		r.refreshAnalogPins(); // Cache the Analog pin information
 		int tempsum=0;
 		for(int x=0;x<numbmes;x++) {
-			AnalogPin temp = r.getAnalogPin(x);
+			AnalogPin temp = r.getAnalogPin(0);
 			tempsum+=temp.getValue();
 			//temp reading takes 3 seconds max
 			r.sleep(3000/numbmes);
